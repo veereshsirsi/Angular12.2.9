84be1c67bb5776cd7be166aba048584111eaac98
veereshsirsi <veereshsirsi@gmail.com>
2021-10-10 23:33:55 +0530

    fresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { OneComponent } from './main/one/one.component';
+import { ParentComponent } from './main/one/parent/parent.component';
+import { ChildComponent } from './main/one/child/child.component';
+import { Children1Component } from './main/one/children1/children1.component';
+import { Children2Component } from './main/one/children2/children2.component';
+import { FiltersPipe } from './filters.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    OneComponent,
+    ParentComponent,
+    ChildComponent,
+    Children1Component,
+    Children2Component,
+    FiltersPipe
   ],
   imports: [
     BrowserModule
